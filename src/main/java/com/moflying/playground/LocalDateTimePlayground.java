c341ce5cb181e3c614ba59ed9a2df66638290b28
zxjsdp <zxjsdp@gmail.com>
2016-09-12 17:39:36 +0800

    Add method for generating range of LocalDate

--- src/main/java/com/moflying/playground/LocalDateTimePlayground.java
+++ src/main/java/com/moflying/playground/LocalDateTimePlayground.java
@@ -2,8 +2,13 @@ package com.moflying.playground;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocalDateTimePlayground {
+    /**
+     * 获取两个日期间的天数，日期减法
+     */
     private static void playLocalDateTime() {
         LocalDate currentDate = LocalDate.parse("2016-07-28");
         // 活动开始的日期
@@ -15,7 +20,38 @@ public class LocalDateTimePlayground {
 
         System.out.println(daySamePeriodAgoBeforeActivity);
     }
+
+    /**
+     * 生成两个日期间的所有日期 [startDate, endDate)
+     *
+     * @param startDate 起始日期
+     * @param endDate 结束日期
+     * @return 两个日期间的所有日期
+     */
+    private static List<LocalDate> generateLocalDateWithinRange(LocalDate startDate, LocalDate endDate) {
+        List<LocalDate> dateWithinRange = new ArrayList<>();
+        if (null == startDate || null == endDate || !startDate.isBefore(endDate)) {
+            return dateWithinRange;
+        }
+
+        int i = 0;
+        while (startDate.plusDays(i).isBefore(endDate)) {
+            dateWithinRange.add(startDate.plusDays(i));
+            i += 1;
+        }
+
+        // StartDate: 2016-09-12
+        //   EndDate: 2016-09-16
+        // DateRange: [2016-09-12, 2016-09-13, 2016-09-14, 2016-09-15]
+        System.out.println("StartDate: " + startDate);
+        System.out.println("  EndDate: " + endDate);
+        System.out.println("DateRange: " + dateWithinRange);
+
+        return dateWithinRange;
+    }
+
     public static void main(String[] args) {
-        playLocalDateTime();
+//        playLocalDateTime();
+        generateLocalDateWithinRange(LocalDate.now(), LocalDate.now().plusDays(4));
     }
 }
