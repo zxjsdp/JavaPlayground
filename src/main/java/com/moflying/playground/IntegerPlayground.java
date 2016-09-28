03d4d06de4db812fb8958bd06a2a0482bced3e9d
zxjsdp <zxjsdp@gmail.com>
2016-09-28 15:01:40 +0800

    Add IntegerPlayground

---
+++ src/main/java/com/moflying/playground/IntegerPlayground.java
@@ -0,0 +1,20 @@
+package com.moflying.playground;
+
+public class IntegerPlayground {
+    /**
+     * 测试使用 `Integer.parseInt` 解析字符串中的 Double 值
+     */
+    private static void intParseDouble() {
+        String num = "8.7";
+        try {
+            System.out.println(Integer.parseInt(num));
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+            System.out.println(Double.parseDouble(num));
+        }
+    }
+
+    public static void main(String[] args) {
+        intParseDouble();
+    }
+}
