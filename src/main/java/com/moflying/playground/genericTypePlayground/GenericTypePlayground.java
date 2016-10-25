ac2f3f3a49f51bfa2c95d099b34ed4ac827032dd
zxjsdp <zxjsdp@gmail.com>
2016-10-26 02:43:09 +0800

    Add generic play example

--- src/main/java/com/moflying/playground/genericTypePlayground/GenericTypePlayground.java
+++ src/main/java/com/moflying/playground/genericTypePlayground/GenericTypePlayground.java
@@ -1,5 +1,10 @@
 package com.moflying.playground.genericTypePlayground;
 
+import com.moflying.playground.utils.CollectionUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GenericTypePlayground {
     // 泛型方法
     public static <T> T getObject(Class<T> c) throws Exception {
@@ -54,14 +59,45 @@ public class GenericTypePlayground {
         System.out.println(c3);
     }
 
+    /**
+     * 检查多个应该为正整数的 Integer, Long, Double 值 (shopId, activityId, userId, ...)
+     *
+     * @param tList 包含多个应该为正整数的 Integer, Long, Double 值
+     */
+    public static <T> void verifyPositiveNumbers(List<T> tList) {
+        if (CollectionUtil.isBlank(tList) || tList.contains(null)) {
+            System.out.println("Invalid list. Blank list or null value in list.");
+            return;
+        }
+
+        tList.forEach(GenericTypePlayground::verifyPositiveNumber);
+    }
+
+    public static <T> void verifyPositiveNumber(T t) {
+        if (null == t) {
+            System.out.printf("%s type is null.\n", t.getClass());
+        }
+        String numString = t.toString();
+        try {
+            if (0 >= Double.parseDouble(numString)) {
+                System.out.printf("%s type value %s less equals than 0\n", t.getClass(), t.toString());
+            }
+        } catch (Exception e) {
+            System.out.printf("Failed to parse %s type value: %s\n", t.getClass(), t.toString());
+        }
+    }
+
     public static void main(String[] args) {
 //        try {
 //            GenericTypePlayground.getObject(Animal.class);
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-        GenericTypePlayground genericTypePlayground = new GenericTypePlayground();
-        genericTypePlayground.playContainer();
+//        GenericTypePlayground genericTypePlayground = new GenericTypePlayground();
+//        genericTypePlayground.playContainer();
+
+        verifyPositiveNumber(new Integer(7));
+        verifyPositiveNumbers(Arrays.asList(new Long(-2), null, new Double(5.4), new Integer(-3), new Double(6.2)));
     }
 }
 
